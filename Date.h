f7205717f7fc36c7590dbaeb3f141addfc24e8d7
Kat Nguyen <katherinenguyen8989@gmail.com>
2026-03-24 21:55:13 -0500

    Add files via upload

---
+++ Date.h
@@ -0,0 +1,25 @@
+#ifndef DATE_H
+#define DATE_H
+#include <iostream> 
+using namespace std; 
+
+class Date {
+    public:
+        Date (int d, int m, int y);
+        
+        int getDay() const;
+        int getMonth() const;
+        int getYear() const;
+
+        int getAge() const;
+        bool operator<(const Date& other) const; 
+        
+        void display() const;
+        
+        
+        private: 
+        int day;
+        int month; 
+        int year;
+};
+#endif
