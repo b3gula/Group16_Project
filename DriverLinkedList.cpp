854eef97cb1943d25ec29901058f7007f378289f
NoodlesCup <DucNgo@my.unt.edu>
2026-03-26 23:16:20 -0500

    Added DriverLL.h and cpp

--- DriverLinkedList.cpp
+++ DriverLinkedList.cpp
@@ -1,1 +1,51 @@
 #include "DriverLinkedList.h"
+#include <iostream>
+
+DriverLinkedList::DriverLinkedList() {
+    head = nullptr;
+    size = 0;
+}
+
+void DriverLinkedList::InsertFront(DriverNode* drivernodeVal) {
+    drivernodeVal->next = head; // direct access
+    head = drivernodeVal;
+    size++;
+}
+
+void DriverLinkedList::InsertBack(DriverNode* drivernodeVal) {
+    if (!head) {
+        head = drivernodeVal;
+    } else {
+        DriverNode* temp = head;
+        while (temp->next != nullptr) {
+            temp = temp->next;
+        }
+        temp->next = drivernodeVal;
+    }
+    size++;
+}
+
+void DriverLinkedList::InsertAfter(string county, DriverNode* drivernodeVal) {
+    DriverNode* temp = head;
+    while (temp != nullptr) {
+        if (temp->data.getCounty() == county) {
+            drivernodeVal->next = temp->next;
+            temp->next = drivernodeVal;
+            size++;
+            return;
+        }
+        temp = temp->next;
+    }
+    InsertBack(drivernodeVal);
+}
+
+DriverNode* DriverLinkedList::Search(string license_idVal) {
+    DriverNode* temp = head;
+    while (temp != nullptr) {
+        if (temp->data.getLicenseID() == license_idVal) {
+            return temp;
+        }
+        temp = temp->next;
+    }
+    return nullptr;
+}
