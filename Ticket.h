2ec276e4dcb0f79b4b4e4f88b11e90b048256e10
Bach Nguyen <bachnguyen2317@gmail.com>
2026-03-28 00:54:29 -0500

    NewTicketString

--- Ticket.h
+++ Ticket.h
@@ -3,16 +3,14 @@
 #include "Date.h"
 #include "Address.h"
 #include <string>
-
 class Ticket {
 private:
     Date ticket_date;
-    Address ticket_location;
+    std::string ticket_location;
     std::string infraction_type;
 public:
     Ticket();
-    Ticket(Date d, Address loc, std::string infraction);
+    Ticket(Date d, std::string loc, std::string infraction);
     void display() const;
 };
-
 #endif
