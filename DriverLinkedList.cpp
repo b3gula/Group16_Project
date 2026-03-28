c40d3b65174c223c97ce774d8815891f1b25865f
NoodlesCup <DucNgo@my.unt.edu>
2026-03-27 22:19:51 -0500

    updated

--- DriverLinkedList.cpp
+++ DriverLinkedList.cpp
@@ -1,23 +1,36 @@
 #include "DriverLinkedList.h"
 #include <iostream>
+using namespace std;
 
-DriverLinkedList::DriverLinkedList() {
+DriverLinkedList::DriverLinkedList(){
     head = nullptr;
     size = 0;
 }
 
-void DriverLinkedList::InsertFront(DriverNode* drivernodeVal) {
-    drivernodeVal->next = head; // direct access
+DriverLinkedList::~DriverLinkedList(){
+    DriverNode* traverse = head;
+    while(traverse){
+        DriverNode* next = traverse->next;
+        delete traverse;
+        traverse = next;
+    }
+    head = nullptr;
+    size = 0;
+}
+
+void DriverLinkedList::InsertFront(DriverNode *drivernodeVal){
+    drivernodeVal->next = head;
     head = drivernodeVal;
     size++;
 }
 
-void DriverLinkedList::InsertBack(DriverNode* drivernodeVal) {
-    if (!head) {
+void DriverLinkedList::InsertBack(DriverNode *drivernodeVal){
+    if(head == nullptr){
         head = drivernodeVal;
-    } else {
-        DriverNode* temp = head;
-        while (temp->next != nullptr) {
+    }
+    else{
+        DriverNode *temp = head;
+        while(temp->next!=nullptr){
             temp = temp->next;
         }
         temp->next = drivernodeVal;
@@ -25,27 +38,51 @@ void DriverLinkedList::InsertBack(DriverNode* drivernodeVal) {
     size++;
 }
 
-void DriverLinkedList::InsertAfter(string county, DriverNode* drivernodeVal) {
-    DriverNode* temp = head;
-    while (temp != nullptr) {
-        if (temp->data.getCounty() == county) {
-            drivernodeVal->next = temp->next;
-            temp->next = drivernodeVal;
+void DriverLinkedList::InsertAfter(string county, DriverNode *drivernodeVal){
+    if(head == nullptr){
+        head = drivernodeVal;
+    }
+    DriverNode *traverse = head;
+    while(traverse!=nullptr){
+        if(traverse->data.getCounty() == county){
+            drivernodeVal->next = traverse->next;
+            traverse->next = drivernodeVal;
             size++;
             return;
         }
-        temp = temp->next;
+        traverse = traverse->next;
     }
     InsertBack(drivernodeVal);
 }
 
-DriverNode* DriverLinkedList::Search(string license_idVal) {
-    DriverNode* temp = head;
-    while (temp != nullptr) {
-        if (temp->data.getLicenseID() == license_idVal) {
-            return temp;
-        }
-        temp = temp->next;
+void DriverLinkedList::Remove(string license_numVal){
+    if (head == nullptr){
+        return;
     }
-    return nullptr;
+
+    if(head->data.getLicenseNum() == license_numVal){
+        DriverNode* temp = head;
+        head = head->next;
+        delete temp;
+        size--;
+        return;
+    }
+    
+    DriverNode* traverse = head;
+    while(traverse->next != nullptr){
+        if(traverse->next->data.getLicenseNum() == license_numVal){
+            DriverNode* temp = traverse->next;
+            traverse->next = temp->next;
+            delete temp;
+            size--;
+            return;
+        }
+        traverse = traverse->next;
+    }
+}
+
+
+
+bool DriverLinkedList::empty(){
+    return head == nullptr;
 }
