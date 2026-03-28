7a29410c14c32ddd24e0dffdfaa5e7146ae4297d
Bach Nguyen <bachnguyen2317@gmail.com>
2026-03-28 00:54:12 -0500

    NewTicketString

--- Ticket.cpp
+++ Ticket.cpp
@@ -1,15 +1,13 @@
 #include "Ticket.h"
 #include <iostream>
-
-Ticket::Ticket() : infraction_type("None") {}
-
-Ticket::Ticket(Date d, Address loc, std::string infraction) 
+Ticket::Ticket() : ticket_location("None"), infraction_type("None") {}
+Ticket::Ticket(Date d, std::string loc, std::string infraction) 
     : ticket_date(d), ticket_location(loc), infraction_type(infraction) {}
-
 void Ticket::display() const {
     if (infraction_type != "None") {
         std::cout << "Ticket: " << infraction_type << " on ";
         ticket_date.display();
+        std::cout << " at " << ticket_location << std::endl;
     } else {
         std::cout << "No tickets.";
     }
