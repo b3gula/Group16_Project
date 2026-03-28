da7c42c3c09f877094633485bb90f02b70585d4e
Bach Nguyen <bachnguyen2317@gmail.com>
2026-03-27 21:54:21 -0500

    Update DriverLinkedList.h

--- DriverLinkedList.h
+++ DriverLinkedList.h
@@ -1,53 +1,37 @@
-#ifndef DRIVER_H
-#define DRIVER_H
-#include "Address.h"
-#include "Date.h"
-#include "Ticket.h"
+#ifndef DRIVER_LL_H
+#define DRIVER_LL_H
+#include "Driver.h"
 #include <string>
 using namespace std;
 
-enum AgeCatg { YOUTH, MIDDLE_AGED, SENIOR};
-enum WorkCatg { STUDENT, GOV_EMPLOYEE, SELF_EMPLOYED, BUSINESS_OWNER, PRIVATE_EMPLOYEE};
-enum ExperienceCatg { NEW_DRIVER, MODERATE_DRIVER, HIGHLY_EXPERIENCED};
-enum MedicalCond { FIT, VISION_IMPAIRED, UPPER_EXTREMITY_IMPAIRMENT, LOCOMOTOR_DISABILITY};
+class DriverNode{
+    public:
+        DriverNode *next;
+        Driver data;
 
-class Driver {
-public:
-    void setName(string nameVal);
-    void setLicense(string licenseVal);
-    void setDOB(Date dobVal);
-    void setLicenseDate(Date licDateVal);
-    void setAddress(Address addrVal);
-    void setTicket(Ticket ticketVal);
+        DriverNode(){
+            next = nullptr;
+        }
 
-    void setAgeCatg(AgeCatg ageVal);
-    void setExperienceCatg(ExperienceCatg expVal);
-    void setWorkCatg(WorkCatg workVal);
-    void setMedicalCond(MedicalCond medVal);
-
-    string getCounty();
-    string getLicenseNum();
-
-    void display();
-
-private:
-    string name;       
-    string license_num;
-    Ticket ticket;
-    AgeCatg age;
-    ExperienceCatg exp;
-    WorkCatg work;
-    MedicalCond med;
-
-    Address address;
-
-    Date DOB;
-    Date licenseDate;
+        DriverNode(Driver *driverVal){
+            data = *driverVal;
+            next = nullptr;
+        }
 };
 
-string AgeCatgToString(AgeCatg ageVal);
-string WorkCatgToString(WorkCatg workVal);
-string ExperienceCatgToString(ExperienceCatg expVal);
-string MedicalCondToString(MedicalCond medVal);
+class DriverLinkedList{
+    private:
+        DriverNode *head;
+        int size;
+
+    public:
+        DriverLinkedList();
+        ~DriverLinkedList();
+        void InsertFront(DriverNode *drivernodeVal);
+        void InsertBack(DriverNode *drivernodeVal);
+        void InsertAfter(string county, DriverNode *drivernodeVal);
+        DriverNode* Search(string license_idVal);
+        bool empty();
+};
 
 #endif
